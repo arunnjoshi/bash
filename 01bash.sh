88639603a687aa5a88f0535f910ee84dd0fc3bee
Arun Joshi <arun.joshi@outlook.in>
2023-08-04 23:46:22 +0530

    #comment expression in bash with var

--- 01bash.sh
+++ 01bash.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-
+# variables in bash
 name="joshi"
 echo "hello, my  name is $name"
 echo $(date)
 echo $USER
+
+# expression in bash
+expr 10 + 20
+expr 10 - 20
+expr 30 / 20
+expr 10 \* 20
+#  var and expression
+num1=10
+num2=20
+expr $num1 + $num2
